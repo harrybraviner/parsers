fe906984c84fcc0a41d51e7bfa4921cefc1fe1e2
Harry J. Braviner <harry.braviner@gmail.com>
2017-02-02 22:03:32 +0000

    Monad instance
    In Parsers3 I've created a version where Parser is an instance
    of Monad. I'm a bit confused as to whether or not I could have
    done this without having to resort to changing Parser to be a type
    rather than a type synonym.

--- Parsers3.hs
+++ Parsers3.hs
@@ -1,44 +1,72 @@
-{-# LANGUAGE TypeSynonymInstances #-}
-
 data ParserResult a = Success(a, String) | Failure deriving Show
 
-type Parser a = String -> ParserResult a
+data Parser a = Parser (String -> ParserResult a)
 
 instance Functor ParserResult where
     fmap f Failure          = Failure
     fmap f (Success(x, xs)) = Success(f x, xs)
 
 instance Functor Parser where
-    fmap f p = \stream -> case p stream of
+    fmap f (Parser p) =
+        Parser(
+            \stream -> case p stream of
                             Failure -> Failure
                             Success(x, xs) -> Success(f x, xs)
+        )
+
+instance Applicative Parser where
+    pure x =
+        Parser(
+            \stream -> Success(x, stream)
+        )
+    (<*>) (Parser f) (Parser p) =
+        Parser(
+            \stream ->
+                case f stream of
+                    Failure        -> Failure
+                    Success(f, xs) -> case p xs of
+                                        Failure        -> Failure
+                                        Success(x, xs) -> Success(f x, xs)
+        )
+           
+instance Monad Parser where
+    (>>=) (Parser p) f =
+        Parser(
+            \stream ->
+                case p stream of
+                    Failure        -> Failure
+                    Success(x, xs) -> ff xs where Parser ff = f x
+        )
 
 return :: a -> Parser a
-return x = \stream -> Success(x, stream)
+return x = Parser (\stream -> Success(x, stream))
 
 bind :: Parser a -> (a -> Parser b) -> Parser b
-bind p1 f =
-    \stream ->
+bind (Parser p1) f =
+    Parser (\stream ->
         case p1 stream of
             Failure        -> Failure
-            Success(x, xs) -> (f x) xs
+            Success(x, xs) -> ff xs where Parser ff = f x
+    )
 
 -- |If the stream starts with a c, returns Success, otherwise Failure
 charParser :: Char -> Parser Char
 charParser c =
-    \stream ->
+    Parser (\stream ->
         case stream of
             x : xs -> if (x == c) then Success(c, xs)
                                   else Failure
             _      -> Failure
+    )
 
 -- |If the first parser succeeds, use its result, otherwise use the second parser
 eitherCombinator :: Parser a -> Parser a -> Parser a
-eitherCombinator p1 p2 =
-    \stream ->
+eitherCombinator (Parser p1) (Parser p2) =
+    Parser (\stream ->
         case p1 stream of
             Failure -> p2 stream
             res     -> res
+    )
 
 -- |Succeeds if the stream begins with one of the decimal digits
 digitParser :: Parser Char
@@ -46,19 +74,22 @@ digitParser = foldl1 eitherCombinator $ map charParser ['0'..'9']
 
 -- |Applies f to the result of the parser, if it succeeds
 applyCombinator :: Parser a -> (a -> b) -> Parser b
-applyCombinator p f =
-    \stream ->
+applyCombinator (Parser p) f =
+    Parser (\stream ->
         case p stream of
             Failure        -> Failure
             Success(x, xs) -> Success(f x, xs)
+    )
 
 -- |Applies the parser as many times as possible
 manyCombinator :: Parser a -> Parser ([a])
-manyCombinator p =
-    \stream ->
+manyCombinator (Parser p) =
+    Parser( \stream ->
         case p stream of
             Failure        -> Success([], stream)
-            Success(x, xs) -> applyCombinator (manyCombinator p) (\ys -> x : ys) xs
+            Success(x, xs) -> pp xs
+                              where (Parser pp) = applyCombinator (manyCombinator (Parser p)) (\ys -> x : ys)
+    )
 
 -- |Parses integers
 integerParser :: Parser Integer
